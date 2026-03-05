c11073944b9614ba9b3c62a55c8bc59c55f3f7c6
Ievlev Rostislav <rostic2004@gmail.com>
2026-03-05 16:07:16 +0300

    ex4

--- src/User.java
+++ src/User.java
@@ -1,46 +1,42 @@
 import java.util.*;
 
 public class User {
+    private Integer id;
     private String name;
     private Integer age;
-    private String job;
-    private String address;
-    private static List<String> VALIDJOBS = Arrays.asList("Google", "Uber", "Amazon");
-    private static List<String> VALID_ADDRESSES = Arrays.asList("London", "New York", "Amsterdam");
+    private Set<String> activites;
 
-    public User(String uname, Integer uage, String ujob, String uaddress){
-        if (!uname.isEmpty()){
-            if (uage>=18) {
-                if (VALIDJOBS.contains(ujob)) {
-                    if (VALID_ADDRESSES.contains(uaddress)) {
-                        this.name = uname;
-                        this.age = uage;
-                        this.job = ujob;
-                        this.address = uaddress;
-                    } else {
-                        throw new IllegalArgumentException("Not valid address");
-                    }
-                } else {
-                    throw new IllegalArgumentException("Not valid job");
-                }
-            } else {
-                throw new IllegalArgumentException("Not valid age");
-            }
-        } else {
-            throw new IllegalArgumentException("Name is empty");
-        }
+    public User(Integer id, String name, Integer age, Set<String> activities){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.activites = activities;
     }
 
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> any_activities){
+        Map<User, String> mapusers = new HashMap<>();
+        for (User user: users){
+            for (String active: user.activites){
+                if (any_activities.contains(active)){
+                    mapusers.put(user, active);
+                    break;
+                }
+            }
+        }
+        return mapusers;
+    }
     public static void main(String[] args){
-        User user1 = new User("Oleg", 19, "Google", "London");
-        User user2 = new User("Alex", 20, "Uber", "New York");
-        User user3 = new User("Igor", 18, "Amazon", "Amsterdam");
-        User user4 = new User("Mira", 19, "Uber", "London");
+        User user1 = new User(1, "Oleg",  20, Set.of("Snowboard", "Shooting"));
+        User user2 = new User(2, "Alex",  21, Set.of("Windsurfing", "Reading"));
+        User user3 = new User(3, "Igor",  23, Set.of("Surfing", "Shooting"));
+        User user4 = new User(4, "Mira",  18, Set.of("Snowboard", "Windsurfing"));
         List<User> userlist = new ArrayList<>();
         userlist.add(user1);
         userlist.add(user2);
         userlist.add(user3);
         userlist.add(user4);
-        System.out.println(userlist);
+        Set<String> activities = Set.of("Shooting", "Windsurfing", "Surfing", "Snowboard", "Reading");
+        Map<User, String> mapuser = findHobbyLovers(userlist, activities);
+        System.out.println(mapuser);
     }
 }
